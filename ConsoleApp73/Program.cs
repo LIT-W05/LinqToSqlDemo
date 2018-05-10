5a0f90e683fbc2f6b54b5c0eebb5439d0ba2deab
Avrumi Friedman <alex+5@lakewoodprogramming.com>
2018-05-09 22:01:45 -0400

    Initial commit

--- ConsoleApp73/Program.cs
+++ ConsoleApp73/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,35 @@ namespace ConsoleApp73
     {
         static void Main(string[] args)
         {
+            PeopleDataContext context = new PeopleDataContext();
+            context.Log = Console.Out;
+
+            Person oldest = context.Persons.OrderBy(p => p.Age).First();
+            oldest.LastName = " and I'm youngest";
+            context.SubmitChanges();
+
+            //foreach (Person older in context.Persons.Where(p => p.Age > 35 && !p.FirstName.StartsWith("a")))
+            //{
+            //    Console.WriteLine(older.FirstName + " " + older.Age);
+            //}
+
+            //Console.WriteLine(context.Persons.Average(p => p.Age));
+
+            //context.Log = Console.Out;
+            //Person person = new Person();
+            //person.FirstName = "Avrumi";
+            //person.LastName = "Friedman";
+            //person.Age = 9876;
+            //context.Persons.InsertOnSubmit(person);
+            //context.SubmitChanges();
+            //Console.WriteLine($"Person added, new id: {person.Id}");
+
+            //foreach (People p in context.Peoples)
+            //{
+            //    Console.WriteLine(p.FirstName);
+            //}
+
+            Console.ReadKey(true);
         }
     }
 }
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PeopleLinqToSql.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var repo = new PeopleRepository(Properties.Settings.Default.ConStr);
+            return View(repo.GetPeople());
+        }
+
+        [HttpPost]
+        public ActionResult Add(Person person)
+        {
+            var repo = new PeopleRepository(Properties.Settings.Default.ConStr);
+            repo.Add(person);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var repo = new PeopleRepository(Properties.Settings.Default.ConStr);
+            return View(repo.GetById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Update(Person person)
+        {
+            var repo = new PeopleRepository(Properties.Settings.Default.ConStr);
+            repo.Update(person);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var repo = new PeopleRepository(Properties.Settings.Default.ConStr);
+            repo.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
